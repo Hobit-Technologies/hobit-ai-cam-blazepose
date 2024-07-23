364c0f7a9e7ba073d81009f190e0ee3f69148015
Himanshu Saini <hs913271@gmail.com>
2024-07-23 12:22:05 +0530

    removed blue dots from body

--- script.js
+++ script.js
@@ -1,135 +1,167 @@
-const videoElement = document.getElementById('video');
+const videoElement = document.getElementById("video");
 videoElement.muted = true;
 videoElement.playsInline = true;
-const canvasElement = document.getElementById('canvas');
-const canvasCtx = canvasElement.getContext('2d');
+const canvasElement = document.getElementById("canvas");
+const canvasCtx = canvasElement.getContext("2d");
 
 let url = new URL(window.location.href);
 let params = new URLSearchParams(url.search);
 
-const isFullScreen = params.get('isFullScreen') || false;
-const isBackCamera = params.get('isBackCamera') || false;
+const isFullScreen = params.get("isFullScreen") || false;
+const isBackCamera = params.get("isBackCamera") || false;
 
 if (isFullScreen) {
-    canvasCtx.canvas.width = window.innerWidth;
-    canvasCtx.canvas.height = window.innerHeight;
+  canvasCtx.canvas.width = window.innerWidth;
+  canvasCtx.canvas.height = window.innerHeight;
 }
 
 function onWindowResized() {
-    if (isFullScreen) {
-        canvasCtx.canvas.width = window.innerWidth;
-        canvasCtx.canvas.height = window.innerHeight;
-        canvasCtx.canvas.style.width = '100vw';
-        canvasCtx.canvas.style.height = '100vh';
-    }
+  if (isFullScreen) {
+    canvasCtx.canvas.width = window.innerWidth;
+    canvasCtx.canvas.height = window.innerHeight;
+    canvasCtx.canvas.style.width = "100vw";
+    canvasCtx.canvas.style.height = "100vh";
+  }
 }
 
-window.addEventListener('resize', onWindowResized);
-
+window.addEventListener("resize", onWindowResized);
 
 const POSE_CONNECTIONS = [
-    [8, 5],
-    [5, 0],
-    [0, 2],
-    [2, 7],
-    [10, 9],
-    [20, 18],
-    [20, 16],
-    [18, 16],
-    [16, 22],
-    [16, 14],
-    [14, 12],
-    [12, 11],
-    [11, 13],
-    [13, 15],
-    [15, 17],
-    [15, 19],
-    [15, 21],
-    [19, 17],
-    [12, 24],
-    [11, 23],
-    [24, 23],
-    [24, 26],
-    [23, 25],
-    [26, 28],
-    [25, 27],
-    [28, 32],
-    [28, 30],
-    [32, 30],
-    [27, 31],
-    [27, 29],
-    [29, 31],
+  [8, 5],
+  [5, 0],
+  [0, 2],
+  [2, 7],
+  [10, 9],
+  [20, 18],
+  [20, 16],
+  [18, 16],
+  [16, 22],
+  [16, 14],
+  [14, 12],
+  [12, 11],
+  [11, 13],
+  [13, 15],
+  [15, 17],
+  [15, 19],
+  [15, 21],
+  [19, 17],
+  [12, 24],
+  [11, 23],
+  [24, 23],
+  [24, 26],
+  [23, 25],
+  [26, 28],
+  [25, 27],
+  [28, 32],
+  [28, 30],
+  [32, 30],
+  [27, 31],
+  [27, 29],
+  [29, 31],
 ];
 
 let lastTimestamp = performance.now();
 let frameCount = 0;
 
 function onResults(results) {
-    const now = performance.now();
-    const elapsed = now - lastTimestamp;
-    let fps = 0;
-    frameCount++;
+  const now = performance.now();
+  const elapsed = now - lastTimestamp;
+  let fps = 0;
+  frameCount++;
 
-    if (elapsed > 1000) {
-        lastTimestamp = now;
-        fps = frameCount / (elapsed / 1000);
-        console.log(`FPS: ${fps.toFixed(2)}`);
-        frameCount = 0;
-    }
+  if (elapsed > 1000) {
+    lastTimestamp = now;
+    fps = frameCount / (elapsed / 1000);
+    console.log(`FPS: ${fps.toFixed(2)}`);
+    frameCount = 0;
+  }
 
-    canvasCtx.save();
-    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    // flip horizontally for mirrored view
-    if (!isBackCamera) {
-        canvasCtx.translate(canvasElement.width, 0);
-        canvasCtx.scale(-1, 1);
-    }
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  // flip horizontally for mirrored view
+  if (!isBackCamera) {
+    canvasCtx.translate(canvasElement.width, 0);
+    canvasCtx.scale(-1, 1);
+  }
 
-    canvasCtx.drawImage(
-        results.image, 0, 0, canvasElement.width, canvasElement.height);
-    if (results.poseLandmarks) {
-        // console.log(results.poseLandmarks);
-        for (let i = 0; i < results.poseLandmarks.length; i++) {
-            // console.log(`Landmark ${i}: (${results.poseLandmarks[i].x}, ${results.poseLandmarks[i].y})`);
-        }
-        window?.ReactNativeWebView?.postMessage(JSON.stringify(
-            {
-                pose: JSON.stringify(results.poseLandmarks),
-                // fps: fps.toFixed(2)
-            }
-        ));
-        drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-                       {color: 'white', lineWidth: 4});
-        drawLandmarks(canvasCtx, results.poseLandmarks,
-                      {color: 'white', lineWidth: 2, radius: 4});
+  canvasCtx.drawImage(
+    results.image,
+    0,
+    0,
+    canvasElement.width,
+    canvasElement.height
+  );
+  if (results.poseLandmarks) {
+    // console.log(results.poseLandmarks);
+    for (let i = 0; i < results.poseLandmarks.length; i++) {
+      // console.log(`Landmark ${i}: (${results.poseLandmarks[i].x}, ${results.poseLandmarks[i].y})`);
     }
-    canvasCtx.restore();
+    window?.ReactNativeWebView?.postMessage(JSON.stringify(results.poseLandmarks));
+    // change
+    // drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+    //                {color: 'white', lineWidth: 4});
+    console.log(results.poseLandmarks);
+
+    const facialPointsIndexes = new Set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    const filteredLandmarks = results.poseLandmarks.filter(
+      (_, index) => !facialPointsIndexes.has(index)
+    );
+    // drawLandmarks(canvasCtx, filteredLandmarks, {
+    //     color: "#777777",
+    //     lineWidth: 2,
+    //     radius: 10,
+    //   });
+    
+    // drawLandmarks(canvasCtx, filteredLandmarks, {
+    //     color: "rgba(133, 251, 255, 0.2)",
+    //     lineWidth: 2,
+    //     radius: 12,
+    //   });
+
+    // blue dots
+    // drawLandmarks(canvasCtx, filteredLandmarks, {
+    //     color: "rgba(133, 251, 255, 0.6)",
+    //     lineWidth: 2,
+    //     radius: 12,
+    //   });
+    // drawLandmarks(canvasCtx, filteredLandmarks, {
+    //   color: "#85FBFF",
+    //   lineWidth: 2,
+    //   radius: 4,
+    // });
+    
+  }
+  canvasCtx.restore();
 }
 
-const pose = new Pose({locateFile: (file) => {
+const pose = new Pose({
+  locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-}});
+  },
+});
 
 pose.setOptions({
-    modelComplexity: 0,
-    smoothLandmarks: true,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
+  modelComplexity: 0,
+  smoothLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5,
 });
 
 pose.onResults(onResults);
 
 new Camera(videoElement, {
-    onFrame: async () => {
-        await pose.send({image: videoElement});
-    },
-    width: 1080,
-    height: 720,
-    facingMode: isBackCamera ? 'environment' : 'user'
-}).start().then(() => {
+  onFrame: async () => {
+    await pose.send({ image: videoElement });
+  },
+  width: 1080,
+  height: 720,
+  facingMode: isBackCamera ? "environment" : "user",
+})
+  .start()
+  .then(() => {
     if (videoElement.readyState >= 3) {
-        videoElement.play();
+      videoElement.play();
     } else {
-        videoElement.addEventListener('canplay', () => videoElement.play());
+      videoElement.addEventListener("canplay", () => videoElement.play());
     }
-});
+  });
